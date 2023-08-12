0b27da03f9a21da344e0d9fbc7ef2633cfe6650c
João Seixas <joaoseixas88@gmail.com>
2023-08-12 10:41:52 -0300

    test: ensure EmailValidator.isValid called with correct params

--- tests/presentation/utils/email-validator.spec.ts
+++ tests/presentation/utils/email-validator.spec.ts
@@ -7,16 +7,21 @@ jest.mock('validator', () => ({
 	}
 }))
 
+const sut = new EmailValidatorAdapter()
+
 describe('EmailValidator Adapter', () => {
 	test('Should return false if validator returns false', () => {
 		jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
-		const sut = new EmailValidatorAdapter()
 		const isValid = sut.isValid('invalid_email@mail.com')
 		expect(isValid).toBe(false)
 	})
 	test('Should return true if validator returns true', () => {
-		const sut = new EmailValidatorAdapter()
 		const isValid = sut.isValid('valid_email@gmail.com')
 		expect(isValid).toBe(true)
+	})
+	test('Should call validator with correct email', () => {
+		const isEmailSpy = jest.spyOn(validator, 'isEmail')
+		sut.isValid('valid_email@gmail.com')
+		expect(isEmailSpy).toBeCalledWith('valid_email@gmail.com')
 	})
 })
