797f6e9ee19f13da1b96765b6b0e3d4880c5ae1a
João Seixas <joaoseixas88@gmail.com>
2023-08-12 23:49:29 -0300

    refactor: move salt constant to outside the factory method

--- tests/infra/cryptograph/bcrypt-adapter.spec.ts
+++ tests/infra/cryptograph/bcrypt-adapter.spec.ts
@@ -6,21 +6,21 @@ jest.mock('bcrypt', () => ({
 }));
 
 
+const salt = 12
 const makeSut = () => {
-	const salt = 12
 	const sut = new BcryptAdapter(salt)
-	return { sut, salt }
+	return { sut }
 }
 describe('Bcrypt Adapter', () => {
 	test('Should call bcrypt with correct value', async () => {
-		const { sut, salt } = makeSut()
+		const { sut } = makeSut()
 		const hashSpy = jest.spyOn(bcrypt, 'hash')
 		await sut.encrypt('any_value')
 		expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
 	})
 
 	test('Should return a hash on success', async () => {
-		const { sut, salt } = makeSut()
+		const { sut } = makeSut()
 		const hash = await sut.encrypt('any_value')
 		expect(hash).toEqual('hashed_value')
 
