a54d8a86b3cf40204dc9cb73c9376a8425cbdf78
João Seixas <joaoseixas88@gmail.com>
2023-08-14 17:27:01 -0300

    refactor: name json body parser test

--- tests/main/middlewares/body-parser.test.ts
+++ tests/main/middlewares/body-parser.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import app from '@/main/config/app'
 
 describe('Body Parser Middleware', () => {
-	test('', async () => {
+	test('Should parse body as json', async () => {
 		app.post('/test_body_parser', (req, res) => {
 			res.send(req.body)
 		})
