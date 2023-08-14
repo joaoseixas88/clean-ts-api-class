1868476fbd4be96593fa93c9b403a45067cf794f
João Seixas <joaoseixas88@gmail.com>
2023-08-14 17:25:54 -0300

    ensure express use json middleware

---
+++ src/main/config/app.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import setupMiddleWares from './middlewares'
+const app = express()
+setupMiddleWares(app)
+
+
+
+
+export default app
---
+++ tests/main/middlewares/body-parser.test.ts
@@ -0,0 +1,13 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+
+describe('Body Parser Middleware', () => {
+	test('', async () => {
+		app.post('/test_body_parser', (req, res) => {
+			res.send(req.body)
+		})
+		await request(app).post('/test_body_parser')
+			.send({ name: 'Rodrigo' })
+			.expect({ name: 'Rodrigo' })
+	})
+})
